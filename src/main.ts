703b8cbf8c6e704ef292e7e7e29dfbef67487739
22051988Di <vitalii.soloninko@gmail.com>
2025-02-05 15:54:35 +0100

    0.9	Add Swagger. npm i @nestjs/swagger swagger-ui-express

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('REST API Waldorf Menu')
+    .setDescription('Documentation REST API Waldorf Menu')
+    .setVersion('1.0.0')
+    .addTag('Vitalii Soloninko')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
 
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
