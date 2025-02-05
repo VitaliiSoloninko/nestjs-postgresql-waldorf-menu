b26e0fa229dad8ed2023d8e4d085746fc63aa62e
22051988Di <vitalii.soloninko@gmail.com>
2025-02-05 11:39:43 +0100

    0.2	First service and controller

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,13 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Server started on port = 5000';
+  }
+
+  getFoods() {
+    return [
+      { id: 1, name: 'Pizza' },
+      { id: 2, name: 'Burger' },
+    ];
   }
 }
