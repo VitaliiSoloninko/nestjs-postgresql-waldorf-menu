0a212b74fe8968675a6666900eacdb22c67af75a
22051988Di <vitalii.soloninko@gmail.com>
2025-02-10 13:46:17 +0100

    1.8	Add standart Role USER

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { Role } from 'src/roles/roles.model';
+import { RolesModule } from 'src/roles/roles.module';
+import { UserRoles } from 'src/roles/user-roles.model';
 import { UsersController } from './users.controller';
 import { User } from './users.model';
 import { UsersService } from './users.service';
-import { Role } from 'src/roles/roles.model';
-import { UserRoles } from 'src/roles/user-roles.model';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [SequelizeModule.forFeature([User, Role, UserRoles])],
+  imports: [SequelizeModule.forFeature([User, Role, UserRoles]), RolesModule],
 })
 export class UsersModule {}
