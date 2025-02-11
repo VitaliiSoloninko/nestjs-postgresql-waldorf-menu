6287d89c1d09d3fc54aebedceccb1e7aa809067c
22051988Di <vitalii.soloninko@gmail.com>
2025-02-11 15:45:26 +0100

    2.1	Registration full form

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -7,4 +7,37 @@ export class UpdateUserDto extends PartialType(CreateUserDto) {
 
   @ApiProperty({ example: 'password', description: 'Password' })
   readonly password: string;
+
+  @ApiProperty({ example: 'John', description: 'First name' })
+  readonly firstName: string;
+
+  @ApiProperty({ example: 'Doe', description: 'Last name' })
+  readonly lastName: string;
+
+  @ApiProperty({ example: 'John', description: 'First name of the child' })
+  readonly firstNameChild: string;
+
+  @ApiProperty({ example: 'Doe', description: 'Last name of the child' })
+  readonly lastNameChild: string;
+
+  @ApiProperty({ example: 'Street', description: 'Street' })
+  readonly street: string;
+
+  @ApiProperty({ example: '1', description: 'Number' })
+  readonly number: string;
+
+  @ApiProperty({ example: '12345', description: 'Postal code' })
+  readonly postalCode: string;
+
+  @ApiProperty({ example: 'City', description: 'City' })
+  readonly city: string;
+
+  @ApiProperty({ example: 'School', description: 'School' })
+  readonly school: string;
+
+  @ApiProperty({ example: '1', description: 'Class' })
+  readonly class: string;
+
+  @ApiProperty({ example: 'A', description: 'Letter' })
+  readonly letter: string;
 }
--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -40,6 +40,83 @@ export class User extends Model<User, UserCreationAttrs> {
   })
   password: string;
 
+  @ApiProperty({ example: 'John', description: 'First name' })
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  firstName: string;
+
+  @ApiProperty({ example: 'Doe', description: 'Last name' })
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  lastName: string;
+
+  @ApiProperty({ example: 'John', description: 'First name of the child' })
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  firstNameChild: string;
+
+  @ApiProperty({ example: 'Doe', description: 'Last name of the child' })
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  lastNameChild: string;
+
+  @ApiProperty({ example: 'Street', description: 'Street' })
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  street: string;
+
+  @ApiProperty({ example: '1', description: 'Number' })
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  number: string;
+
+  @ApiProperty({ example: '19053', description: 'Postal code' })
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  postalCode: string;
+
+  @ApiProperty({ example: 'Schwerin', description: 'City' })
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  city: string;
+
+  @ApiProperty({ example: 'Waldorf', description: 'School' })
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  school: string;
+
+  @ApiProperty({ example: '1', description: 'Class' })
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  class: string;
+
+  @ApiProperty({ example: 'A', description: 'Letter' })
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  letter: string;
+
   @BelongsToMany(() => Role, () => UserRoles)
   roles: Role[];
 }
